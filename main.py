66e9d71e900d7285572096158329bf92a55712c5
Ortega2k <diego@8alpha.ai>
2025-09-10 13:39:44 -0600

    Pygame

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def add(a, b):
 
 result = add(a, b)
 print("The sum is:", result)
+
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+pygame.display.set_caption('My Pygame Window')
